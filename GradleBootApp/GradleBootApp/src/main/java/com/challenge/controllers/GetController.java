ae4d920e433fd5a62991558571d6dbe5f2d6d76d
logriffith <logriffith1991@gmail.com>
2021-06-03 19:04:28 -0600

    added Spring Boot Application class and dependencies

---
+++ GradleBootApp/GradleBootApp/src/main/java/com/challenge/controllers/GetController.java
@@ -0,0 +1,10 @@
+package com.challenge.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/get")
+public class GetController {
+
+}
