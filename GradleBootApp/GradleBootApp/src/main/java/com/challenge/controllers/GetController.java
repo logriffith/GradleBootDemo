e15a66437108101e4e296604518a0436b34efd4c
logriffith <logriffith1991@gmail.com>
2021-06-03 19:36:29 -0600

    worked on a controller and created an Angular project

--- GradleBootApp/GradleBootApp/src/main/java/com/challenge/controllers/GetController.java
+++ GradleBootApp/GradleBootApp/src/main/java/com/challenge/controllers/GetController.java
@@ -1,10 +1,27 @@
 package com.challenge.controllers;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
-@RequestMapping(value = "/get")
+@RequestMapping(value = "/challenge")
 public class GetController {
+	
+	@GetMapping(value = "/info")
+	public ResponseEntity<List<String>> getAppInfo(){
+		LocalDate currentDate = LocalDate.now();
+		List<String> info = new ArrayList<>();
+		info.add(currentDate.toString());
+		info.add("appName");
+		return ResponseEntity.ok(info);
+	}
 
 }
